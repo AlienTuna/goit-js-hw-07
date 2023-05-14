73111fedea41ab4b8af462b76d1fb48927df1c35
AlienTuna <101868546+AlienTuna@users.noreply.github.com>
2023-05-14 23:52:28 +0300

    hw-07-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,54 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const parrentGalleryEl = document.querySelector('ul.gallery');
+// создание разметки галереи
+parrentGalleryEl.insertAdjacentHTML('beforeend', createGallery(galleryItems));
+function createGallery(items) {
+
+    return items.map(({ preview, original, description }, ind) => {
+        return `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </li>
+        `;
+    }).join('');
+}
+
+let bigImgEl;
+// прослушивание события при нажатии на изображение (делигирование)
+parrentGalleryEl.addEventListener('click', onImageClick);
+
+function onImageClick(e) {
+    e.preventDefault();
+    const TARGET_TYPE = 'IMG';
+    const isImg = e.target.nodeName === TARGET_TYPE;
+    if (!isImg) {
+        return;
+    }
+
+    const currentImgAdress = e.target.getAttribute('data-source')
+
+    bigImgEl = basicLightbox.create(`
+        <img src="${currentImgAdress}" width="800" height="600">
+    `).show()
+
+    // document.addEventListener('keydown', onEscapeKeyDown)
+}
+
+// function onEscapeKeyDown(e) {
+
+//     console.log(e.code);
+//     if (e.code !== 'Escape'){
+//         return 'Not escape'
+//     }
+//     bigImgEl.close();
+//     document.removeEventListener('keydown', onEscapeKeyDown);
+
+// }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const parrentGalleryEl = document.querySelector('ul.gallery');
+// создание разметки галереи
+parrentGalleryEl.insertAdjacentHTML('beforeend', createGallery(galleryItems));
+function createGallery(items) {
+
+    return items.map(({ preview, original, description }, ind) => {
+        return `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </li>
+        `;
+    }).join('');
+}
+
+// прослушивание события при нажатии на изображение (делигирование)
+parrentGalleryEl.addEventListener('click', onImageClick);
+
+function onImageClick(e) {
+    e.preventDefault();
+    const TARGET_TYPE = 'IMG';
+    const isImg = e.target.nodeName === TARGET_TYPE;
+    if (!isImg) {
+        return;
+    }
+    const lightbox = new SimpleLightbox('.gallery a', {
+        captionsData: 'alt',
+        captionDelay: 250,
+     });
+
+}
+
