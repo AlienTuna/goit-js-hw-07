36f00649f6bb770b4da0a43acf4b8df529ca36f1
AlienTuna <101868546+AlienTuna@users.noreply.github.com>
2023-05-15 11:23:29 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -21,20 +21,8 @@ function createGallery(items) {
     }).join('');
 }
 
-// прослушивание события при нажатии на изображение (делигирование)
-parrentGalleryEl.addEventListener('click', onImageClick);
-
-function onImageClick(e) {
-    e.preventDefault();
-    const TARGET_TYPE = 'IMG';
-    const isImg = e.target.nodeName === TARGET_TYPE;
-    if (!isImg) {
-        return;
-    }
-    const lightbox = new SimpleLightbox('.gallery a', {
-        captionsData: 'alt',
-        captionDelay: 250,
-     });
-
-}
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+ });
 
